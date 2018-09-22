9e4ec862be126a69abd6fb02275d801a0dbbdd41
HyunJungOh <hjoh@selva-i.co.jp>
2018-09-22 12:35:01 +0900

    simple react component- adding messages on the list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      messages: ["first message","second message", "third message"],
+      input : '',
+    }
+  }
+
+ handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  handleClick = (e) =>{
+    // retrieving data from state
+    var data = [];
+    data = this.state.input;
+    // pass the state to the handleSubmit
+    this.handleSubmit(data)
+  }
+
+  handleSubmit = (data) => {
+    this.setState({
+//      messages: this.state.messages.concat(data)
+      messages: [...this.state.messages, data]
+    })
+      console.log(data)
+    }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +40,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={this.handleChange} value={this.state.input}></input>
+        <button onClick={this.handleClick}>submit</button>
+          <ul>
+            {/*map function*/}
+            {this.state.messages.map(message=>{
+              return <li>{message}</li>
+            }
+            )}
+          </ul>
       </div>
     );
   }
